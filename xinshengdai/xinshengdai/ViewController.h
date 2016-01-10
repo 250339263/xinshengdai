50f49479320c55ff10aea079f9dd862085e0cffa
cxb <250339263@qq.com>
2016-01-10 22:47:56 +0800

    初始化代码

---
+++ xinshengdai/xinshengdai/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xinshengdai
+//
+//  Created by cxb on 16/1/10.
+//  Copyright © 2016年 chuxiaobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
