fc459216d0d2965757f48c307a9a4ad47ba6ba52
cxb <250339263@qq.com>
2016-01-11 19:21:25 +0800

    测试管理

---
+++ xinshengdai/xinshengdai/add.h
@@ -0,0 +1,13 @@
+//
+//  add.h
+//  xinshengdai
+//
+//  Created by cxb on 16/1/11.
+//  Copyright © 2016年 chuxiaobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface add : UIView
+
+@end
---
+++ xinshengdai/xinshengdai/add.m
@@ -0,0 +1,21 @@
+//
+//  add.m
+//  xinshengdai
+//
+//  Created by cxb on 16/1/11.
+//  Copyright © 2016年 chuxiaobo. All rights reserved.
+//
+
+#import "add.h"
+
+@implementation add
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
